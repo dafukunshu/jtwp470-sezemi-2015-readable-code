1c54e58c52b90c9b8528b7d129e977231186bdfe
fukuda <fukuda@Dummy-no-MacBook-Pro.local>
2015-06-06 14:56:03 +0900

    spec6: 解決:範囲外のidも動いてしまう問題

--- main.py
+++ main.py
@@ -15,12 +15,18 @@ if __name__ == "__main__":
     with open("recipe-data.txt", "r") as recipes:
         for id, recipe in enumerate(recipes):
             recipe_table[id] = recipe
-
+            print(recipe_table) ##
     if len(sys.argv) <= 1:
         dump_table()
     else:
+        argv_len = len(sys.argv)
+        print(id)
+        if id < 0 or argv_len > id:
+            print("error") 
+            sys.exit(1)
+        
         # TODO: idが範囲外のときにErrorなどを出す refer to memo.md
         # TODO: id が正しくないものを出力してしまうのを直す
         args_id = sys.argv[1]
         print("{id}: {recipe}".format(
-            id=str(args_id), recipe=recipe_table.get(id)))
+            id=str(args_id), recipe=recipe_table.get(int(args_id))))
