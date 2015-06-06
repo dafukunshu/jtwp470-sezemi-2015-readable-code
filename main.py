74f2bc7c552dd42ae16bf5a835f75af453004ce6
Ryosuke Sato <rskjtwp@gmail.com>
2015-06-06 11:40:22 +0900

    spec5: レシピにIDを振りました

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+# レシピにIDをふった辞書 ex. id:recipe_name (idはint)
+recipe_table = dict()
+
+
+def dump_table():
+    for id, recipe in recipe_table.items():
+        print("{id}: {recipe}".format(id=str(id), recipe=recipe), end="")
+
+
 if __name__ == "__main__":
     with open("recipe-data.txt", "r") as recipes:
-        for recipe in recipes:
-            print(recipe)
+        for id, recipe in enumerate(recipes):
+            recipe_table[id] = recipe
+
+    dump_table()
