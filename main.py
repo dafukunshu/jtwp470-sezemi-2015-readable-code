ab74d02c60f6fde9540200cede8d09fefca903e7
Ryosuke Sato <rskjtwp@gmail.com>
2015-06-06 11:16:40 +0900

    spec3: レシピを読み込む仕様に対応

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-print("オムライス")
+if __name__ == "__main__":
+    with open("recipe-data.txt", "r") as recipes:
+        for recipe in recipes:
+            print(recipe)
